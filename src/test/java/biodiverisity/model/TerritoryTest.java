810bf2e7d589db157a95064c46367b7f9f35758c
maaciekj <maciejjarzewicz@wp.pl>
2021-06-09 16:18:04 +0200

    TerritoryTest complete

--- src/test/java/biodiverisity/model/TerritoryTest.java
+++ src/test/java/biodiverisity/model/TerritoryTest.java
@@ -7,9 +7,9 @@ import biodiversity.view.TerritoryObserver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 public class TerritoryTest {
 
@@ -31,10 +31,6 @@ public class TerritoryTest {
                 .observer(observer)
                 .counter(counter)
                 .build();
-        organism = Mockito.mock(Organism.class);
-        Mockito.when(organism.getRow()).thenReturn(1);
-        Mockito.when(organism.getCol()).thenReturn(1);
-        territory.addInhabitant(organism);
     }
 
 
@@ -48,7 +44,37 @@ public class TerritoryTest {
 
     @Test
     public void invokingDoItsTurnInTerritoryShouldInvokeDoItsTurnInOrganism() {
+        organism = mockOrganism('a', 1, 1);
+        territory.addInhabitant(organism);
         territory.doItsTurn();
         Mockito.verify(organism, Mockito.times(1)).doItsTurn();
+    }
+
+    private Organism mockOrganism(char sign, int row, int col) {
+        Organism organism1 = Mockito.mock(Organism.class);
+        Mockito.when(organism1.getSign()).thenReturn(sign);
+        Mockito.when(organism1.getRow()).thenReturn(row);
+        Mockito.when(organism1.getCol()).thenReturn(col);
+        return organism1;
+    }
+
+    @Test
+    public void checkFreePlacesShouldReturnPlacesListOfExpectedSize(){
+        organism = mockOrganism('a', 1, 1);
+        territory.addInhabitant(organism);
+        List<Field> freeFields = territory.checkFreePlaces(organism.getRow(), organism.getCol(), 1);
+        assertEquals(freeFields.size(), 8);
+    }
+
+    @Test
+    public void checkOrganismNearbyShouldNotIncludeOwnSpecies(){
+        Organism organism1 = mockOrganism('a', 1, 1);
+        Organism organism2 = mockOrganism('a', 0, 1);
+        Organism organism3 = mockOrganism('b', 2, 2);
+        territory.addInhabitant(organism1);
+        territory.addInhabitant(organism2);
+        territory.addInhabitant(organism3);
+        List<Organism> organismsNearby = territory.checkOrganismsNearbyExcludingOwnSpecies(organism1.getRow(), organism1.getCol(), 3, organism1.getSign());
+        assertEquals(1, organismsNearby.size());
     }
 }
