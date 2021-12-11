e9ba818259b14a6a2b615426ba1ee2c832fd9689
maaciekj <maciejjarzewicz@wp.pl>
2021-12-11 18:48:02 +0100

    update log4j in pom, starting ReplicationStrategyTest

---
+++ src/test/java/biodiverisity/model/organism/behavior/replication_strategies/ReplicationStrategyTest.java
@@ -0,0 +1,31 @@
+package biodiverisity.model.organism.behavior.replication_strategies;
+
+import biodiversity.model.organism.Organism;
+
+import biodiversity.model.organism.behavior.replication_strategies.ReplicationStrategy;
+import biodiversity.model.territory.NumberGenerator;
+import biodiversity.model.territory.Territory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ReplicationStrategyTest {
+
+
+    private Territory territory;
+    private Organism organism;
+    private ReplicationStrategy replicationStrategy;
+    private NumberGenerator numberGenerator;
+
+
+
+    @Test
+    public void replicationStrategyShouldProduceACopyOfSameSpecies(){
+
+        assertTrue(true);
+
+    }
+
+
+
+}
